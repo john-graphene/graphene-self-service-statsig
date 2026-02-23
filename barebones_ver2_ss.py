92af1c24569a3f657b01580825324615cc3664c0
John Lee <john@graphenesvc.com>
2026-02-23 13:49:40 +0800

    bug fix: strip % suffix

--- barebones_ver2_ss.py
+++ barebones_ver2_ss.py
@@ -1484,7 +1484,7 @@ def apply_stat_sig(slide, statsig_type, base, decimal_place=0, slide_type="Perfo
                 # print("table_idx>=row_start_index:",slide_table_df.iat[table_idx, col_start_index], row)
                 num_list = list(row[col_start_index:len(row)])
                 # num_list = [float(num.strip("%")) for num in num_list if num != "-"]
-                num_list = [0 if num =="-" else float(num) for num in num_list]
+                num_list = [0 if num =="-" else float(num.strip().strip("%")) for num in num_list]
                 # num_list = [0 if num =="-" else num for num in num_list]
 
                 # benchmark_idx = get_benchmark_col_idx(slide_table_df, ref_benchmark) - col_start_index
